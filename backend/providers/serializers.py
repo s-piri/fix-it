ac45178570436b48c733fdc1aced2cc69535cddb
S. Piri <snyt.piri@gmail.com>
2025-08-31 06:35:26 +1000

    add missing job_type return

--- backend/providers/serializers.py
+++ backend/providers/serializers.py
@@ -4,4 +4,4 @@ from .models import Provider
 class ProviderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Provider
-        fields = ['provider_id', 'provider_name', 'eta', 'profile_picture', 'jobs', 'rating', 'vehicle', 'is_available']
+        fields = ['provider_id', 'provider_name', 'job_type', 'eta', 'hourly_rate', 'profile_picture', 'jobs', 'rating', 'vehicle', 'is_available']
